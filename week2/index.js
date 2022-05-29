a0bccd4fe461e0b416e21eb08f3d8c44f2a31183
Omina Sharifzoda <omina.sharifzoda@bryanuniversity.edu>
2022-05-29 16:09:10 -0600

    Practice1

---
+++ week2/index.js
@@ -0,0 +1,13 @@
+
+var newP = document.createElement("p")
+newP.textContent = "“All of this was created with Javascript”"
+newP.style.textAlign="center"
+document.body.appendChild(newP)
+
+var newH1=document.createElement("h1")
+newH1.textContent="“Welcome to my JS site”"
+newH1.style.textAlign="center"
+document.body.appendChild(newH1)
+
+var newListItem= document.createElement("li")
+newListItem.textContent
