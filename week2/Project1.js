0293a704aa84b7d2553b70efd841257a7e48b11f
Omina Sharifzoda <omina.sharifzoda@bryanuniversity.edu>
2022-05-29 20:40:57 -0600

    Project1

---
+++ week2/Project1.js
@@ -0,0 +1,39 @@
+var newH1= document.createElement("h1")
+newH1.textContent="Amazon sale."
+document.body.appendChild (newH1)
+
+var newP = document.createElement("p")
+newP.textContent ="Automatically apply Amazon coupon codes for free - it's kinda genius."
+document.body.appendChild(newP)
+
+var newList=document.createElement("ul")
+newList.textContent = "List"
+document.body.appendChild(newList)
+
+var newLi1= document.createElement("li")
+newLi1.textContent="Use Capital One Shopping for Free."
+newList.appendChild (newLi1)
+
+var newLi2= document.createElement("li")
+newLi2.textContent="Save effortlessly."
+newList.appendChild (newLi2)
+
+var newLi3= document.createElement("li")
+newLi3.textContent="Shop as you normally do."
+newList.appendChild (newLi3)
+
+var newFooter= document.createElement("footer")
+newFooter.textContent="Links:"
+document.body.appendChild (newFooter)
+
+var newNavbar=document.createElement("nav")
+newNavbar.textContent= "https://www.amazon.com/ref=nav_logo"
+document.body.appendChild(newNavbar)
+
+var newNavbar=document.createElement("nav")
+newNavbar.textContent= "https://capitaloneshopping.com/instant/Amazon?utm_source=Dolphin&utm_campaign=132422076196&utm_term=598774482462&gclid=CjwKCAjws8yUBhA1EiwAi_tpER7Gn8pGH8qpCbuyIWJPFd06qV8YvBLB1U241YQgHxto8Y05-onYSxoCwvsQAvD_BwE"
+document.body.appendChild(newNavbar)
+
+
+
+
