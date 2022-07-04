b1ae060ab296ce2a3e370f539ea20eb87a9ce602
Omina Sharifzoda <omina.sharifzoda@bryanuniversity.edu>
2022-07-04 00:00:53 -0600

    Correction

--- week2/index.js
+++ week2/index.js
@@ -1,13 +1,25 @@
+var newH1=document.createElement("h1")
+newH1.textContent="“Welcome to my JS site”"
+newH1.style.textAlign="center"
+document.body.appendChild(newH1)
 
 var newP = document.createElement("p")
 newP.textContent = "“All of this was created with Javascript”"
 newP.style.textAlign="center"
 document.body.appendChild(newP)
 
-var newH1=document.createElement("h1")
-newH1.textContent="“Welcome to my JS site”"
-newH1.style.textAlign="center"
-document.body.appendChild(newH1)
+var newListItem= document.createElement("ol")
+newListItem.textContent=("List")
+document.body.appendChild(newListItem)
 
-var newListItem= document.createElement("li")
-newListItem.textContent
+var newLiItem1=document.createElement("li")
+newLiItem1.textContent=("Practice")
+newListItem.appendChild(newLiItem1)
+
+var newLiItem2=document.createElement("li")
+newLiItem2.textContent=("Practice")
+newListItem.appendChild(newLiItem2)
+
+var newLiItem3=document.createElement("li")
+newLiItem3.textContent=("Practice")
+newListItem.appendChild(newLiItem3)
